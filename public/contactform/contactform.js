e227e41c1bf88d9172c91b0058968e02f0fae34d
daniuribe25 <dani.uribe25@gmail.com>
2018-03-15 09:26:46 -0500

    header in response

--- public/contactform/contactform.js
+++ public/contactform/contactform.js
@@ -79,7 +79,10 @@ jQuery(document).ready(function($) {
         else var str = {name: $('#name').val(),cel: $('#cel').val(),subject: $('#subject').val(),message: $('#message').val()}	
             $.ajax({
                 type: "POST",
-                headers: {  'Access-Control-Allow-Origin': '*' },
+                headers: {  
+                        'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
+                        'Access-Control-Allow-Origin': '*' 
+                },
                 // url: "http://localhost:5000/api/sendMail",
                 url: "https://maestraisis.herokuapp.com/api/sendMail",
                 data: str,
