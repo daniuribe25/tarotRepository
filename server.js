b2dfe9cfefa30353d2ca00fb97d1577a4c942972
daniuribe25 <dani.uribe25@gmail.com>
2018-03-15 09:08:03 -0500

    header in response

--- server.js
+++ server.js
@@ -27,6 +27,10 @@ router.route('/').get(function(req,res){
 
 router.route('/sendMail').post(function(req,res){
     sendUserEmail(req.body)
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     res.send("OK");
 });
 
